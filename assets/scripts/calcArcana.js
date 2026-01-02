207cbc2b402edd2c03fed1d9aa941741ad1b6d59
Henrique4754 <107286472+Henrique4754@users.noreply.github.com>
2026-01-02 20:32:19 -0300

    implementação da funcionalidade de histórico de atributos na calculadora

--- assets/scripts/calcArcana.js
+++ assets/scripts/calcArcana.js
@@ -30,7 +30,7 @@ function pericia() {
     valorNivelSub
   );
 }
-
+// Função de realizar o envio do valor preenchido ao apertar enter.
 document
   .getElementById("valorDado")
   .addEventListener("keydown", function (event) {
@@ -41,22 +41,35 @@ document
       document.getElementById("calcularBtnAtbr").click();
     }
   });
-
+// Função de calcular o valor de atributos inserido pelo usuário e armazenar em uma <ul> com limite para 6 valores armazenados.
 document
   .getElementById("calcularBtnAtbr")
   .addEventListener("click", function () {
     const diceValueInput = document.getElementById("valorDado");
     const diceValue = parseInt(diceValueInput.value);
     const totalPointsBruto = (diceValue - 10) / 2;
-
     const totalPointsLimpo = Math.floor(totalPointsBruto);
 
+    
+// função para armazenar os valores inseridos no input dentro da <ul>
+    function listaAtbr() {
+      const atbrList = document.getElementById("ultimosValoresList");
+      let novoItem = document.createElement("li");
+      if (diceValue <= 11) {
+        novoItem.textContent = "1";
+      } else {
+        novoItem.textContent = totalPointsLimpo;
+      }
+      atbrList.appendChild(novoItem);
+    }
+
+    // Conserta o bug de zerar os modificadores, antes com a fórmula de cálculo tirada do livro do jogador, os inputs abaixo de 11 estavam retornando valor zero, quando de acordo com as regras deveria ser 1.
     if (diceValue <= 11) {
-      document.getElementById("atributoFinal").textContent = "1"
+      document.getElementById("atributoFinal").textContent = "1";
+      listaAtbr();
     } else {
       document.getElementById("atributoFinal").textContent = totalPointsLimpo;
+      listaAtbr();
     }
-    document.getElementById('valorDado').textContent= ""
+    document.getElementById("valorDado").value = "";
   });
-
-const numVirg = 23.5;
